7a640b3d15d4373c8cfcc95a42e71e20dab7ca2a
Tux Solbakk <tux@krageroby.no>
2017-08-22 21:37:47 +0200

    Started on key administration.

---
+++ template/account/json/addsshkey.php
@@ -0,0 +1,80 @@
+<?php
+declare(strict_types=1);
+namespace gimle;
+
+function returnError ($errno, $errmsg): void
+{
+	$form = new Form();
+	$form->setProperty('addsshkey', true);
+
+	echo json_encode([
+		'errno' => $errno,
+		'errmsg' => $errmsg,
+		'token' => $form->getId()
+	]);
+}
+
+if ((!isset($_POST['token'])) || (!isset($_POST['title'])) || (!isset($_POST['sshkey']))) {
+	returnError(1, _('Invalid form data.'));
+	return true;
+}
+
+if ($_POST['sshkey'] === '') {
+	returnError(2, _('No key.'));
+	return true;
+}
+
+if (!preg_match('/^ssh-rsa AAAA[0-9A-Za-z+\/]+[=]{0,3} [^@]+@([^@]+)$/', trim($_POST['sshkey']), $matches)) {
+	returnError(2, _('Invalid key.'));
+	return true;
+}
+
+$title = $_POST['title'];
+if ($title === '') {
+	$title = $matches[1];
+}
+if (!filter_var($title, FILTER_VALIDATE_FILENAME)) {
+	returnError(3, _('Invalid title.'));
+	return true;
+}
+
+$checkForm = Form::getInstance($_POST['token']);
+
+if (!$checkForm->validate()) {
+	returnError(4, _('Timeout, try again.'));
+	return true;
+}
+
+$usedTitles = [];
+$usedKeys = [];
+if (file_exists('/home/tux/gitolite-admin/keydir/' . $_SESSION['user'])) {
+	foreach (new \DirectoryIterator('/home/tux/gitolite-admin/keydir/' . $_SESSION['user']) as $fileInfo) {
+		$fileName = $fileInfo->getFilename();
+		if (substr($fileName, 0, 1) === '.') {
+			continue;
+		}
+		$usedTitles[] = $fileName;
+		$fullName = '/home/tux/gitolite-admin/keydir/' . $_SESSION['user'] . '/' . $fileName . '/' . $_SESSION['user'] . '.pub';
+		$usedKeys[] = trim(file_get_contents($fullName));
+	}
+}
+
+if (in_array($title, $usedTitles)) {
+	returnError(5, _('Title in use.'));
+	return true;
+}
+
+if (in_array(trim($_POST['sshkey']), $usedKeys)) {
+	returnError(6, _('Key in use.'));
+	return true;
+}
+
+$form = new Form();
+$form->setProperty('addsshkey', true);
+
+echo json_encode([
+	'title' => $title,
+	'token' => $form->getId(),
+]);
+
+return true;
--- template/dashboard.php
+++ template/dashboard.php
@@ -6,7 +6,35 @@ use \gimle\canvas\Canvas;
 
 Canvas::title(_('Dashboard') . ' | Snotra');
 
+$form = new Form();
+$form->setProperty('signin', true);
+
 ?>
+<script>
+	jQuery(($) => {
+		$('#addKey').on('submit', (e) => {
+			e.preventDefault();
+			var $form = $('#addKey');
+			$.ajax({
+				url: $form.attr('action'),
+				method: $form.attr('method'),
+				data: $form.serialize()
+			}).always((r) => {
+				if (r.token !== undefined) {
+					$('[name="token"]').val(r.token);
+					if (r.errmsg !== undefined) {
+						$('#feedback').show();
+						$('#feedback').html(r.errmsg);
+					}
+				}
+				if (r.title !== undefined) {
+					$('#feedback').hide();
+				}
+				console.log(r);
+			});
+		});
+	});
+</script>
 <header id="topBar">
 	<div style="">
 		<a href="<?=BASE_PATH?>signout">
@@ -15,5 +43,24 @@ Canvas::title(_('Dashboard') . ' | Snotra');
 	</div>
 	<h1><img src="<?=BASE_PATH?>favicon.png"/> Snotra <span>git</span></h1>
 </header>
+
+<main>
+	<h1><?=_('Manage ssh keys')?></h1>
+	<div id="keyList"></div>
+	<form id="addKey" action="<?=MAIN_BASE_PATH?>addsshkey" method="POST">
+		<input type="hidden" name="token" value="<?=$form->getId()?>">
+		<label>
+			<span class="title"><?=_('Title:')?></span>
+			<input name="title" type="text" placeholder="<?=_('my-machine-name')?>"/>
+			<span class="tip"><?=_('If left blank the value will be taken from the key.')?></span>
+		</label>
+		<label>
+			<span class="title"><?=_('Key:')?></span>
+			<textarea name="sshkey" placeholder="ssh-rsa … username@host"></textarea>
+		</label>
+		<div id="feedback"></div>
+		<button><?=_('Add')?></button>
+	</form>
+</main>
 <?php
 return true;
